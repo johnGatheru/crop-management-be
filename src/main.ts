51b332242f41ec8b16767b4b757410668c86e335
jonny <gatherujohn11@gmail.com>
2022-07-07 11:24:09 +0300

    header

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
+import { Response } from 'express';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
@@ -11,6 +12,9 @@ async function bootstrap() {
   });
   app.useStaticAssets(join(__dirname, '..', 'files'), {
     prefix: '/files/',
+  });
+  app.use((req, res: Response, next) => {
+    res.setHeader('Content-Disposition', 'attachment');
   });
   const config = new DocumentBuilder()
     .setTitle('Rex api')
