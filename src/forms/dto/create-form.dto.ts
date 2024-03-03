f9613cb8b4c2d5b548ab60811be2bdde8f7e51bb
jonnyGatheru <jonnyiragu@gmail.com>
2024-03-03 21:22:39 +0300

    modified form entity

--- src/forms/dto/create-form.dto.ts
+++ src/forms/dto/create-form.dto.ts
@@ -1,29 +1,38 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty, IsString } from 'class-validator';
 
 export class CreateFormDto {
+  @ApiProperty()
   @IsNotEmpty()
   @IsString()
   ageBracket: string;
 
+  @ApiProperty()
   @IsNotEmpty()
   @IsString()
   crop1: string;
 
+  @ApiProperty()
   @IsString()
   crop2: string;
 
+  @ApiProperty()
   @IsString()
   crop3: string;
 
+  @ApiProperty()
   @IsString()
   farmAcreage: string;
 
+  @ApiProperty()
   @IsNotEmpty()
   isDemoDone: boolean;
 
+  @ApiProperty()
   @IsString()
   visitNotes: string;
 
+  @ApiProperty()
   @IsString()
-  nextVisit: Date;
+  nextVisit: string;
 }
